5a6d769a3053caf333303313b9944c6f9a10fe00
SirPotatoIV <otoolej33@gmail.com>
2019-12-30 15:54:56 -0600

    Have basic file structure based on express documentation and previous activities. Server currently runs and brings up index.html from the public folder.

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+// require("./routes/api-routes.js")(app);
+
+// Here we introduce HTML routing to serve different HTML files
+require("./routes/html-routes.js")(app);
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
